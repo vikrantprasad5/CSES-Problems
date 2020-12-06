d3ef075182ff38f025f825130c298964cf6d134e
vikrantprasad5 <66894598+vikrantprasad5@users.noreply.github.com>
2020-12-06 16:17:32 +0530

    Update WierdAlgorithms.cpp

--- Introduction/WierdAlgorithms.cpp
+++ Introduction/WierdAlgorithms.cpp
@@ -1,1 +1,27 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+void solve(long  n)
+{
+    cout<<n<<" ";
+    if(n==1)
+    {
+        return;
+    }
+    if(n%2==0)
+    {
+        n=n/2;
+    }
+    else
+    {
+        n=n*3 + 1;
+    }
+    solve(n);
+}
+int main() {
+	// your code goes here
+	long n;
+	cin>>n;
+	solve(n);
+	return 0;
+}
